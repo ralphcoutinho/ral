9b9b39960def0974d99e8604a1edba80b615c554
sdac-11 <sdac-11@sdac-11-PC>
2024-02-14 10:06:39 +0530

    EmpPojo

---
+++ Main/src/EmpPojo.java
@@ -0,0 +1,31 @@
+
+public class EmpPojo {
+private String name;
+private int id;
+private double salary;
+private String Address;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public double getSalary() {
+	return salary;
+}
+public void setSalary(double salary) {
+	this.salary = salary;
+}
+public String getAddress() {
+	return Address;
+}
+public void setAddress(String address) {
+	Address = address;
+} 
+}
